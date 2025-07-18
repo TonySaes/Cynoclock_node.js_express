6f2a6cc1ed79c22ea6c4b91bdb3db060b2d7b257
Tony Saes <saestony.dev@gmail.com>
2025-07-18 16:53:09 +0200

    views & partials files created

--- cynoclock.js
+++ cynoclock.js
@@ -13,7 +13,7 @@ app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index', {dogs});
+    res.render('home', {dogs});
 });
 
 app.listen(port, () => {
